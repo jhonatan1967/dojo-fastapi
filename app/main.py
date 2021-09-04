c259a49cde6135ee2572c2bedc47b8d839869866
jhonatan1967 <jhonatana.torres@udea.edu.co>
2021-09-04 12:43:18 -0500

    Dojo FastAPI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-
 from app.db.config import server
-
+from app.routers.api import router
 app = FastAPI(title="Dojo FastAPI")
-
+app.include_router(router,prefix="/api")
 
 @app.on_event("startup")
 async def startup_event():
